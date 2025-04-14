3738901af2e6f73f7f710c9644d23e792ba6cdbb
Stijn Laurent <46028254+laurentstijn@users.noreply.github.com>
2025-04-14 11:37:04 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+
+// JOUW GEHELE WERKENDE JAVASCRIPT HIER
+// (Het volledige script dat we samen hebben gemaakt.)
+
+// Omdat de inhoud te lang is om in 1 Python blok te zetten,
+// Plaatsen we nu een basisbestand dat je daarna zelf kan aanvullen.
+alert("Project geladen! Vergeet je eigen Firebase-configuratie in te vullen.");
