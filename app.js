544c8cf7c4129a4ee96e74a1bc4550b49823ac01
Stijn Laurent <46028254+laurentstijn@users.noreply.github.com>
2025-04-18 17:01:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-
 // 🔥 Firebase configuratie
 const firebaseConfig = {
   apiKey: "AIzaSyBrvdXyuMpgkC4lFKjQDeHNihzFRbzMANU",
   authDomain: "tekensvg.firebaseapp.com",
-  projectId: "tekensvg"
+  databaseURL: "https://tekensvg-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tekensvg",
+  storageBucket: "tekensvg.firebasestorage.app",
+  messagingSenderId: "180262088073",
+  appId: "1:180262088073:web:6470e50e3ad8a587ef8558"
 };
 
 firebase.initializeApp(firebaseConfig);
