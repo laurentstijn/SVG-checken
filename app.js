26a5da8abc45c0a7634e53957abd821576a932e6
Stijn Laurent <46028254+laurentstijn@users.noreply.github.com>
2025-04-18 10:19:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -592,8 +592,7 @@ lockCheckbox.addEventListener('change', () => {
     function maakNieuweSVG() {
       const svgTemplate = `
         <svg xmlns="http://www.w3.org/2000/svg" width="300" height="300">
-          <rect x="50" y="50" width="200" height="200" fill="lightgray" stroke="black" />
-        </svg>
+
       `;
       container.innerHTML = svgTemplate;
       laatstGebruikteBestandsnaam = ""; // reset naam
